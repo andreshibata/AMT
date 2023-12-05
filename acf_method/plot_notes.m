72deec8f76c6bc2c4bc801ee4745bf8e5c19b86e
Steven Hastings <5508452+tcswp@users.noreply.github.com>
2023-12-05 18:26:48 -0500

    fixed octave issue

--- acf_method/plot_notes.m
+++ acf_method/plot_notes.m
@@ -3,9 +3,9 @@ function plot_notes(notes,durations)
     %arrayfun(@(x) string(x),note_str)
     note_str = join(note_str, ' ')
 
-    score = "\version ""2.24.3"" {\clef treble\time 4/4"+newline+note_str+newline+"}";
+    score = "\version ""2.24.3"" \fixed c' {\clef treble\time 4/4"+newline+note_str+newline+"}";
 
-    ly = ".\lilypond-2.24.3-mingw-x86_64\lilypond-2.24.3\bin\lilypond.exe";
+    ly = "..\..\lilypond-2.24.3-mingw-x86_64\lilypond-2.24.3\bin\lilypond.exe";
     filename = "score.ly";
     file = fopen(filename,'wt');
     fprintf(file,"%s",score);
